8943fe264ee8d94940d366dac8e9b02f23812469
Davidson Xavier <davidson.ax95@gmail.com>
2020-12-31 14:03:16 -0300

    Primeiro Commit

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,14 @@
+//comentário de 1 linha
+console.log("linha 1")
+
+/*
+    Comentário 
+    multiplas linhas
+*/
+console.log("linha 2")
+
+/*
+    * Comentário
+    * Múltiplas linhas
+    */
+console.log("Linha 3")
