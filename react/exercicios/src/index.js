dde83dddfceb3e6a53179fa56b1e490271d8a62c
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-08 21:17:13 -0300

    trabalhando com Multiplos componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -2,6 +2,14 @@ import React from 'react'
 import  ReactDOM  from 'react-dom'
 
 // import Primeiro from'./componentes/Primeiro'
-import BomDia from './componentes/BomDia'
+// import BomDia from './componentes/BomDia'
+import Multi, {BoaNoite} from './componentes/Multiplos'
 
-ReactDOM.render(<BomDia nome="Davidson" />, document.getElementById('root'))
+// ReactDOM.render(<BomDia nome="Davidson" />, document.getElementById('root'))
+
+ReactDOM.render(
+    <div>
+        <Multi.BoaTarde nome="Ana" />
+        <BoaNoite nome ="Bia"/>
+    </div>
+,document.getElementById('root'))
