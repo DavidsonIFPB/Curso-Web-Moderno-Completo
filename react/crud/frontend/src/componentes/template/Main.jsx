d39d619646262e0eab3f59a4646de68caa074728
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-18 21:35:51 -0300

    Criando os principais arquivos do Frontend

---
+++ react/crud/frontend/src/componentes/template/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Main.css'
+import Header from './Header'
+
+export default props=>
+    <React.Fragment>
+        <Header></Header>
+        <main className="content">
+            Principal
+        </main>
+    </React.Fragment>
+
