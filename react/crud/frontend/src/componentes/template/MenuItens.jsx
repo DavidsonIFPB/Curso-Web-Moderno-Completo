c6f518fba9606fc3682ce3249162e8dd275c4f72
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-20 23:19:00 -0300

    implementando A navegação e Os Menus Itens

---
+++ react/crud/frontend/src/componentes/template/MenuItens.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './MenuItens.css'
+
+
+export default props =>
+    <nav className="menu">
+        <a href={`#${props.value ===undefined ? '' :'/'+props.value}`}><i className={`fa fa-${props.icon}`}></i>{props.name}</a>
+    </nav>
--- react/crud/frontend/src/componentes/template/Nav.jsx
+++ react/crud/frontend/src/componentes/template/Nav.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import MenuItens from './MenuItens'
 import './Nav.css'
 
 export default props=>
     <aside className="menu-area">
-        Navegação
+        <MenuItens icon={'home'} name={'Início'}/>
+        <MenuItens icon ={'user'} name={'Usuário'}value="users"/>
     </aside>
