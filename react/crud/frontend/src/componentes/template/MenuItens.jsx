c994826ce4fe92f63a7ddaa6bf8a145d74a2f703
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-21 11:02:15 -0300

    Refatorando, e adicionando responsividade

--- react/crud/frontend/src/componentes/template/MenuItens.jsx
+++ react/crud/frontend/src/componentes/template/MenuItens.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import './MenuItens.css'
 import { Link } from "react-router-dom";
 
 const MenuItens = props =>
-<nav className="menu">
     <Link to={`${props.value ===undefined ? '' :props.value}`}><i className={`fa fa-${props.icon}`}></i> {props.name}</Link>
-</nav>
 
 export default MenuItens
--- react/crud/frontend/src/componentes/template/Nav.jsx
+++ react/crud/frontend/src/componentes/template/Nav.jsx
@@ -4,8 +4,10 @@ import './Nav.css'
 
 const Nav = props=>
 <aside className="menu-area">
-    <MenuItens icon={'home'} name={'Início'}/>
-    <MenuItens icon ={'user'} name={'Usuário'}value="users"/>
+    <nav className="menu">
+        <MenuItens icon={'home'} name={'Início'}/>
+        <MenuItens icon ={'user'} name={'Usuário'}value="users"/>
+    </nav>
 </aside>
 
 export default Nav
