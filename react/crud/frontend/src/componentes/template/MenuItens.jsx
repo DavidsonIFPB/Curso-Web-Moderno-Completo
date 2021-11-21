38dd2c52d6ebbd409c41a23ee2af224479405842
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-20 23:47:47 -0300

    implementando userCrud

--- react/crud/frontend/src/componentes/template/MenuItens.jsx
+++ react/crud/frontend/src/componentes/template/MenuItens.jsx
@@ -4,5 +4,5 @@ import './MenuItens.css'
 
 export default props =>
     <nav className="menu">
-        <a href={`#${props.value ===undefined ? '' :'/'+props.value}`}><i className={`fa fa-${props.icon}`}></i>{props.name}</a>
+        <a href={`#${props.value ===undefined ? '' :'/'+props.value}`}><i className={`fa fa-${props.icon}`}></i> {props.name}</a>
     </nav>
