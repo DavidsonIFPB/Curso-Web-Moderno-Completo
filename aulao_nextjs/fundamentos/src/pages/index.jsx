32d80b5b57f8ba3cc9a429860758113b69679575
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-23 21:30:03 -0300

    Componente com Estado

--- aulao_nextjs/fundamentos/src/pages/index.jsx
+++ aulao_nextjs/fundamentos/src/pages/index.jsx
@@ -4,6 +4,7 @@ export default function Inicio(){
     return (
         <div style={{
             display:'flex',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
             flexWrap: 'wrap',
@@ -13,6 +14,8 @@ export default function Inicio(){
             <Navegador texto="Exemplo" destino="exemplo" cor="#9400D3"/>
             <Navegador texto="JSX" destino="jsx" cor ="crimson"/>
             <Navegador texto="Navegação #01" destino="/navegacao" cor ="green"/>
+            <Navegador texto="Navegação #02" destino="/cliente/sp-2/123" cor ="blue"/>
+            <Navegador texto="Componente com Estado" destino="/estado" cor ="darkturquoise"/>
             
         </div>
     )
