7108e822670125541af0a4ae95c150e8324303fe
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-29 19:56:30 -0300

    implementando botao

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Botao from "../components/Botao";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -5,17 +6,17 @@ import Cliente from "../core/Cliente";
 export default function Home() {
 
   const clientes = [
-    new Cliente('Ana', 34,'1'),
-    new Cliente('Bia', 45,'2'),
-    new Cliente('Carlos', 23,'3'),
-    new Cliente('Pedro', 54,'4')
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4')
   ]
-  
-  function clienteSelecionado (cliente: Cliente) {
+
+  function clienteSelecionado(cliente: Cliente) {
     console.log(cliente.nome)
 
   }
-  function clienteExcluido (cliente: Cliente) {
+  function clienteExcluido(cliente: Cliente) {
     console.log(`Excluir ... ${cliente.nome}`)
 
   }
@@ -25,13 +26,16 @@ export default function Home() {
       flex justify-center items-center h-screen
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
-    `}> 
-     <Layout titulo="Cadastro Simples">
-       <Tabela clientes={clientes}
-        clienteSelecionado={clienteSelecionado}
-        clienteExcluido = {clienteExcluido}
-       ></Tabela>
-     </Layout>
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao className="mb-4" cor='gray'>Novo cliente</Botao>
+        </div>
+        <Tabela clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
+      </Layout>
     </div>
   )
 }
