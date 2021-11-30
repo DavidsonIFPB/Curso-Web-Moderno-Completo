82a5a1e3ac7c563d8bda25dfc1401d95949174cd
Davidson Xavier <davidson.ax95@gmail.com>
2021-11-30 20:05:53 -0300

    implementando Formulario

--- aulao_nextjs/next-crud/src/pages/index.tsx
+++ aulao_nextjs/next-crud/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
@@ -29,12 +30,13 @@ export default function Home() {
     `}>
       <Layout titulo="Cadastro Simples">
         <div className="flex justify-end">
-          <Botao className="mb-4" cor='gray'>Novo cliente</Botao>
+          <Botao className="mb-4" cor='green'>Novo cliente</Botao>
         </div>
-        <Tabela clientes={clientes}
+        {/* <Tabela clientes={clientes}
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
-        ></Tabela>
+        ></Tabela> */}
+        <Formulario cliente={clientes[0]}></Formulario>
       </Layout>
     </div>
   )
