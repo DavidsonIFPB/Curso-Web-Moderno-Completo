aa7809be6d931f53ed27a9228d18ce80d7e413b1
Davidson Xavier <davidson.ax95@gmail.com>
2021-12-22 22:30:04 -0300

    implementando IPC e alterando as versões das deps

--- most-used-words/src/components/Home.vue
+++ most-used-words/src/components/Home.vue
@@ -1,53 +1,57 @@
 <template>
-    <v-container fluid>
-        <v-form>
-            <v-file-input label="Selecione as Legendas"
-             prepend-icon="mdi-message-text"
-             append-outer-icon="mdi-send"
-             outlined
-             multiple 
-             chips
-             v-model="files"
-             @click:append-outer="processSubtitles" />
-
-        </v-form>
-        <div class='pills'>
-            <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount"/>
-
-        </div>
-    </v-container>
+  <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
+    <div class="pills">
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
 </template>
 
 <script>
-
-import Pill from './Pill.vue'
+import { ipcRenderer } from "electron";
+import Pill from "./Pill.vue";
 
 export default {
-    components:{Pill},
-    data: function(){
-        return {
-            files: [],
-            groupedWords:[
-                {name:'i', amount: 1234},
-                {name:'you', amount: 900},
-                {name:'he', amount: 853}
-                ]
-        }
+  components: { Pill },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      const paths = this.files.map(f => f.path)
+
+      ipcRenderer.send("process-subtitles", paths);
+      ipcRenderer.on("process-subtitles", (event, resp) => {
+        this.groupedWords = resp
+      });
     },
-    methods:{
-        processSubtitles(){
-            console.log(this.files)
-        }
-    }
-}
+  },
+};
 </script>
 
 <style>
-
-.pills{
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
-
 </style>
