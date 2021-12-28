8fe02e00adf3ffa1a747473ae763b3580b5769b4
Davidson Xavier <davidson.ax95@gmail.com>
2021-12-28 19:53:43 -0300

    Criando diretivas no intuito de aprendizado

--- Angular-Crud/frontend/src/app/app.module.ts
+++ Angular-Crud/frontend/src/app/app.module.ts
@@ -14,7 +14,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component'
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+
+
 
 
 @NgModule({
@@ -25,6 +29,9 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
+    RedDirective,
+    ForDirective
+
   ],
   imports: [
     BrowserModule,
