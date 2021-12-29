0d3085b75ff0e8423706c0b5419aabada3f1a471
Davidson Xavier <davidson.ax95@gmail.com>
2021-12-29 20:52:31 -0300

    Implementa o metódo que cria produto no backend

--- Angular-Crud/frontend/src/app/components/product/product.service.ts
+++ Angular-Crud/frontend/src/app/components/product/product.service.ts
@@ -1,13 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar}from '@angular/material/snack-bar'
+import { Observable} from "rxjs";
+import { Product } from './product.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private snackBar: MatSnackBar) { }
+  private baseUrl = 'http://127.0.0.1:3001/products'
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
+  create(product: Product): Observable <Product> {
+    return this.http.post<Product>(this.baseUrl, product)
+  }
+  
   showMessage(msg: string) {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
