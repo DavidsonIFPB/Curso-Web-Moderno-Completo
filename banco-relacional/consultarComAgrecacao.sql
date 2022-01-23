44004ad57541f19331dd25805cfa67bae9aadb06
Davidson Xavier <davidson.ax95@gmail.com>
2022-01-22 21:48:15 -0300

    Consulta agregando por região

---
+++ banco-relacional/consultarComAgrecacao.sql
@@ -0,0 +1,15 @@
+SELECT
+    regiao AS 'Região',
+    sum(populacao) AS Total
+FROM
+    estados
+GROUP BY
+    regiao
+ORDER BY
+    Total DESC;
+
+
+SELECT
+    avg(populacao) AS Total
+FROM
+    estados;
