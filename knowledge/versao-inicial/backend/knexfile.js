f655506cb3edea7558b2f37082a0e04d788fe559
Davidson Xavier <davidson.ax95@gmail.com>
2022-02-01 22:17:08 -0300

    Corrige problema com o repositorio

---
+++ knowledge/versao-inicial/backend/knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+
+  client: 'pg',
+  connection: {
+    database: 'knowledge',
+    user: 'postgres',
+    password: '123456'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+
+};
