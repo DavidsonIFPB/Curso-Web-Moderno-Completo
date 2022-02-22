a81f0e84a651db827dce44161ae27fe1caba5512
Davidson Xavier <davidson.ax95@gmail.com>
2022-02-21 21:50:09 -0300

    Cria Paginas de administração

--- knowledge/versao-inicial/frontend/src/components/admin/adminPages.vue
+++ knowledge/versao-inicial/frontend/src/components/admin/adminPages.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="adminPages">
-      <Pagetitle icon ='fa fa-cogs' main='Administração do Sistema' sub='Cadastro & Cia'></Pagetitle>
+    <Pagetitle
+      icon="fa fa-cogs"
+      main="Administração do Sistema"
+      sub="Cadastro & Cia"
+    ></Pagetitle>
+    <div class="admin-Pages-tabs">
+      <b-card no-body>
+        <b-tabs> 
+          <b-tab title="Articles" active>
+            <ArticleAdmin/>
+          </b-tab>
+          <b-tab title="Categories">
+            <CategoryAdmin/>
+          </b-tab>
+
+          <b-tab title="Users">
+            <UserAdmin/>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </div>
   </div>
 </template>
 
 <script>
-import Pagetitle from '@/components/template/Pagetitle'
+import Pagetitle from "@/components/template/Pagetitle";
+import ArticleAdmin from './ArticleAdmin.vue'
+import CategoryAdmin from './CategoryAdmin.vue'
+import UserAdmin from './UserAdmin.vue'
+
+
 export default {
-    name : 'adminPages',
-    components : {Pagetitle}
-}
+  name: "adminPages",
+  components: { Pagetitle, ArticleAdmin, CategoryAdmin, UserAdmin },
+};
 </script>
 
 <style>
-
 </style>
